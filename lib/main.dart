25cb915646dc5eccb9332dbbe973a54c1760162d
nguyenminhtuan238 <minhdu238@gmail.com>
2022-09-13 21:12:23 +0700

    Xay dung trang cac san pham nguoi dung

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:myshop/models/product.dart';
+import 'package:myshop/ui/products/user_product_list_tile.dart';
 import 'ui/products/products_manager.dart';
 import 'ui/products/product_detail_screen.dart';
 import 'ui/products/product_overview_screen.dart';
+import 'ui/products/user_products_screen.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -25,8 +27,8 @@ class MyApp extends StatelessWidget {
      ),
      
    ),
-      home: SafeArea(
-        child: ProductsOverviewScreen(),
+      home: const SafeArea(
+        child: UserProductsScreen(),
       ),
       
       
