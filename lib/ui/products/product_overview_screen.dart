668cafdc1a4b6a88a808aa03adfc6e2df73941a4
nguyenminhtuan238 <minhdu238@gmail.com>
2022-09-19 22:47:13 +0700

    thong tin so san pham trong gio hang

--- lib/ui/products/product_overview_screen.dart
+++ lib/ui/products/product_overview_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:myshop/ui/cart/cart_manager.dart';
 import 'package:myshop/ui/cart/cart_screen.dart';
 import 'products_grid.dart';
 import '../shared//app_drawer.dart';
+import 'top_right_badge.dart';
 enum FilterOptions{favorites,all}
 class ProductsOverviewScreen extends StatefulWidget {
   const ProductsOverviewScreen({super.key});
@@ -27,15 +29,17 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen>{
     
   }
   Widget buildshoppingCartIcon(){
-    return IconButton(
+    return TopRightBadge(
+      data: CartManager().productCount,
+      child: IconButton(
       icon:const Icon(
          Icons.shopping_cart
       ),
       onPressed: (){
         Navigator.of(context).pushNamed(CartScreen.routeName);
       },
+    ),
     );
-    
   }
   Widget buildProductFilterMenu(){
     return PopupMenuButton(
@@ -63,4 +67,5 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen>{
        ],
       );
   }
+  
 }
