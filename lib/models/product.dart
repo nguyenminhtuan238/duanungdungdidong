61ee6b09efe6b4ad799a2dc21d0e521945dcf3da
nguyenminhtuan238 <minhdu238@gmail.com>
2022-10-16 22:02:04 +0700

    Cai dat chuc nang chon san pham yeu thich

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,18 +1,30 @@
+import 'dart:ffi';
+
+import 'package:flutter/foundation.dart';
 class Product{
   final String? id;
   final String title;
   final String description;
   final double price;
   final String imageUrl;
-  final bool isFavorite;
+  final ValueNotifier<bool> _isFavorite;
   Product({
     this.id,
     required this.title,
     required this.description,
     required this.price,
     required this.imageUrl,
-    this.isFavorite=false,
-  });
+    isFavorite=false,
+  }): _isFavorite = ValueNotifier(isFavorite);
+  set isFavorite(bool newvalue){
+    _isFavorite.value=newvalue;
+  }
+  bool get isFavorite{
+    return _isFavorite.value;
+  }
+  ValueNotifier<bool> get isFavoriteListenable{
+    return _isFavorite;
+  }
   Product copyWith({
     String? id,
     String? title,
